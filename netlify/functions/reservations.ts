cdc9f07a10523dd4f80bc37bbcd26c1c35e67ef8
taka-dhj <taka@discoveryhiddenjapan.com>
2025-10-07 23:00:34 +0800

    feat: add Netlify Function stubs

---
+++ netlify/functions/reservations.ts
@@ -0,0 +1,4 @@
+import type { Handler } from '@netlify/functions';
+export const handler: Handler = async () => {
+  return { statusCode: 200, body: JSON.stringify({ reservations: [], total: 0 }) };
+};
